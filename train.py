9ee481e9b8b074e224c4f3a83667804fa7ba62d3
Srinivas Balivada <balivada987@gmail.com>
2020-11-06 07:14:56 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -10,20 +10,12 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+import pandas as pd
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds =  Dataset.Tabular.from_delimited_files(data)### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-x_train,x_test,y_train,y_test = train_test_split(x,y test_size=0.1, random_state=42)
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+from azureml.core import Dataset, Datastore
+data="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds =  Dataset.Tabular.from_delimited_files(path=data)### YOUR CODE HERE ###
+ds.to_pandas_dataframe()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,8 +42,17 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    #return x_df,y_df
-    
+    return x_df,y_df
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.1, random_state=42)
+### YOUR CODE HERE ###a
+
+y_train
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
